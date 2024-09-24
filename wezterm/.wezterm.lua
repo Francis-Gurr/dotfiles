9d134a55c41048b86d71d48d1b8d9affbb374959
Francis-Gurr <francis.gurr@gmail.com>
2024-09-24 20:05:12 +0000

    move gitconfig into dir and update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,9 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Dracula (Official)"
 
-config.font = wezterm.font("Fira Code")
-config.font_size = 18.0
+config.font = wezterm.font("Fira Code Nerd Font")
+config.font_size = 12.0
+
+config.use_fancy_tab_bar = false
 
 return config

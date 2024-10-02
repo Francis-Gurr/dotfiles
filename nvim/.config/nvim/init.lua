8296b8c8fbe9c7402184c2e945fc6b7c208991b8
Francis-Gurr <francis.gurr@pendo.io>
2024-10-02 14:52:11 +0100

    add neovim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,17 @@
+-- Set leader key (before loading plugins)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+
+-- Load options (settings)
+require("options")
+
+-- Load key mappings
+require("keymaps")
+
+-- Load autocomands
+require("autocmds")
+
+-- Bootstrap and configure lazy.nvim plugin manager
+require("lazy") -- This will handle plugin loading

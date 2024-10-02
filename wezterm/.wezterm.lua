de15bf72de3188d71dd2e0f333dc516af39c3b07
Francis-Gurr <francis.gurr@pendo.io>
2024-10-02 14:59:33 +0100

    update wezterm config for pendo mac

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Tokyo Night"
 
 config.font = wezterm.font_with_fallback({ "Fira Code Nerd Font", "Fira Code" })
-config.font_size = 12.0
+config.font_size = 14.0
 
 config.use_fancy_tab_bar = false
 

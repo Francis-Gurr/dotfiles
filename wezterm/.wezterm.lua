af9c8160bef217ad12035c0efdd9442b17c733fe
Francis-Gurr <francis.gurr@pendo.io>
2024-10-09 08:50:30 +0100

    add fallback font to wezterm so when switching profiles on mac wezterm can use fallback

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Tokyo Night"
 
-config.font = wezterm.font("Fira Code Nerd Font")
+config.font = wezterm.font_with_fallback({ "Fira Code Nerd Font", "Fira Code" })
 config.font_size = 12.0
 
 config.use_fancy_tab_bar = false

74347e9e091ff72f3baa11ed03e66702340731c8
Francis-Gurr <francis.gurr@gmail.com>
2025-02-20 09:38:35 +0000

    🐛 fix(nvim/snacks): remove duplicate zoom and zen toggles

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -402,21 +402,7 @@ return {
 		},
 		-- Other
 		{
-			"<leader>z",
-			function()
-				Snacks.zen()
-			end,
-			desc = "Toggle Zen Mode",
-		},
-		{
-			"<leader>Z",
-			function()
-				Snacks.zen.zoom()
-			end,
-			desc = "Toggle Zoom",
-		},
-		{
-			"<leader>.",
+			"<leader>t.",
 			function()
 				Snacks.scratch()
 			end,
